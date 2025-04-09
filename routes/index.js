cb2f9928fc94666863c8f0820a67addd720738cf
wan <conut22@naver.com>
2025-04-09 11:51:14 +0900

    ✨ feat : Create get API to send gridData
    - Created a GET API that retrieves gridData.
    - Currently using a JSON file without a database.
    - Data is shuffled randomly and sliced before being returned.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const router = express.Router();
+
+// 랜덤으로 배열을 섞는 함수 (Fisher-Yates 알고리즘)
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET Grid page. */
+router.get('/news/grid', function (req, res, next) {
+  const filePath = path.join(__dirname, '../public/data/gridData.json');
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send('파일을 읽는 데 오류가 발생했습니다.');
+    }
+
+    const jsonData = JSON.parse(data);
+
+    const shuffledData = shuffleArray(jsonData);
+
+    const slicedData = shuffledData.slice(0, 96);
+
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(slicedData, null, 2));
+  });
 });
 
 module.exports = router;
